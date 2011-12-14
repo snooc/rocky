8f06ad0cbfb5fcc5c90322044fbf766c0382dabb
Cody Coons <cody@codycoons.com>
2011-12-13 22:50:16 -0500

    Rails project made. OHHHH YEAAAAAHHH

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rocky::Application.config.secret_token = '88f0b3cc5fc473e054b2021fd183515efcf703f47f629534b73c62c0b72051b9dca650686e07ac3691d5a9068c82c96f9c2ad755657653e3c47fe7f20ee7f8da'
